1d478afe66d52e184c18bd901b05ed54c4588a47
562681041@qq.com <wobuainiQQ13579>
2018-08-26 22:24:08 +0800

    添加加载图片数组问题

--- src/js/base/ResourceLoader.js
+++ src/js/base/ResourceLoader.js
@@ -5,22 +5,46 @@ export class ResourceLoader {
         this.res = new Resource().imgList
 
         this.map = new Map(this.res)
+        this.imgListLen = 0
         for (let [key, value] of this.map) {
-            const img = new Image()
-            img.src = value
-            this.map.set(key, img)
+            if (typeof value === 'object') {
+                value.forEach((item) => {
+                    this.imgListLen += 1
+                    const img = new Image()
+                    img.src = value
+                    this.map.get(key).push(img)
+                })
+            } else {
+                this.imgListLen += 1
+                const img = new Image()
+                img.src = value
+                this.map.set(key, img)
+            }
+
         }
+
 
     }
 
     onLoad(callback) {
         let count = 0
         for (let value of this.map.values()) {
+            if (value instanceof Array) {
+                value.forEach((item) => {
+                    item.onload = () => {
+                        count++
+                        if (count === this.imgListLen) {
+                            callback(this.map)
+                        }
+                    }
+                })
 
-            value.onload = () => {
-                count++
-                if (count === this.map.size) {
-                    callback(this.map)
+            } else {
+                value.onload = () => {
+                    count++
+                    if (count === this.imgListLen) {
+                        callback(this.map)
+                    }
                 }
             }
         }
